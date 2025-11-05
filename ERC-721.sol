86fc057ae868e2a7720cebb5ea16f5f4aefd3fdd
rikiossas2000 <43321991+rikiossas2000@users.noreply.github.com>
2025-11-06 01:45:55 +0700

    Update ERC-721.sol

--- ERC-721.sol
+++ ERC-721.sol
@@ -1,1 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v4.9.3/contracts/token/ERC721/ERC721.sol";
+
+error HaikuNotUnique();
+error NotYourHaiku(uint id);
+error NoHaikusShared();
+
+contract HaikuNFT is ERC721 {
+    struct Haiku {
+        address author;
+        string line1;
+        string line2;
+        string line3;
+    }
+
+    Haiku[] public haikus;
+    uint public counter = 1;
+
+    mapping(address => uint[]) public sharedHaikus;
+    mapping(bytes32 => bool) private usedLineHash;
+
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+        haikus.push(Haiku(address(0), "", "", "")); // pad index 0
+    }
+
+    function mintHaiku(string calldata l1, string calldata l2, string calldata l3) external {
+        if (usedLineHash[keccak256(bytes(l1))]) revert HaikuNotUnique();
+        if (usedLineHash[keccak256(bytes(l2))]) revert HaikuNotUnique();
+        if (usedLineHash[keccak256(bytes(l3))]) revert HaikuNotUnique();
+
+        usedLineHash[keccak256(bytes(l1))] = true;
+        usedLineHash[keccak256(bytes(l2))] = true;
+        usedLineHash[keccak256(bytes(l3))] = true;
+
+        uint id = counter;
+        _safeMint(msg.sender, id);
+        haikus.push(Haiku(msg.sender, l1, l2, l3));
+        counter = id + 1;
+    }
+
+    function shareHaiku(address _to, uint id) public {
+        if (ownerOf(id) != msg.sender) revert NotYourHaiku(id);
+        sharedHaikus[_to].push(id);
+    }
+
+    function getMySharedHaikus() public view returns (Haiku[] memory) {
+        uint[] storage ids = sharedHaikus[msg.sender];
+        if (ids.length == 0) revert NoHaikusShared();
+        Haiku[] memory list = new Haiku[](ids.length);
+        for (uint i = 0; i < ids.length; i++) {
+            list[i] = haikus[ids[i]];
+        }
+        return list;
+    }
+}
